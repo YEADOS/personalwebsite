526564a6a156df734412b0b4e433f9de2649f621
YEADOS <nicholasyeadon@gmail.com>
2025-05-04 15:49:37 +1000

    fixed smaller mistaked and finally upated project pictures

--- app/extension/background.js
+++ app/extension/background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url && changeInfo.url.includes("https://leetcode.com/")) {
-      fetch("http://localhost:5000/tracker/update_streak", { method: "POST" })
+      fetch("http://nyeados.pythonanywhere.com/tracker/update_streak", { method: "POST" })
         .then(response => console.log("Streak updated"))
         .catch(error => console.error("Error updating streak:", error));
     }
